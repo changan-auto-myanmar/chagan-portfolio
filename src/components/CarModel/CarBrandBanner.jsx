292c3a1d4694c40a21da4e42ead1498fd5cf98c0
aaog2 <aungaungoogame2@gmail.com>
2024-11-26 11:34:51 +0630

    feat: car brand banner err

--- src/components/CarModel/CarBrandBanner.jsx
+++ src/components/CarModel/CarBrandBanner.jsx
@@ -60,10 +60,7 @@ function CarBrandBanner() {
             <div className="brightness-50 bg-black h-full">
               <img src={item.img} className="w-full h-full object-cover" />
             </div>
-            <p
-              className="absolute bottom-[34px] md:bottom-[64px] left-0 z-10 ps-3 md:ps-[64px] text-white text-[20px] md:text-5
-            xl font-changan font-bold hidden lg:block"
-            >
+            <p className="absolute bottom-[34px] md:bottom-[64px] left-0 z-10 ps-3 md:ps-[64px] text-white text-[20px] md:text-5xl font-changan font-bold hidden lg:block">
               {item.name}
             </p>
           </SwiperSlide>
