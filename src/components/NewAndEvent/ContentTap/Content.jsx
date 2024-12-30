cbbe872a88970726e9434d8fcc999ebf289655bf
aaog2 <aungaungoogame2@gmail.com>
2024-12-30 15:13:46 +0630

    feat: chg text style

--- src/components/NewAndEvent/ContentTap/Content.jsx
+++ src/components/NewAndEvent/ContentTap/Content.jsx
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 function Content({ tab }) {
   return (
     <div className="">
-      <div className="px-5 lg:px-0">
+      <div
+        className="px-5 lg
+      :px-0"
+      >
         <img src={tab.image} className="w-full" alt="Placeholder" />
         <div className="flex justify-between items-center px-0 py-4">
           <p className="">{tab.desc}</p>
