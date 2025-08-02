81da4c633d7fce50e30a63e274dd0de8963e1b45
acehidan <aungaungoogame2@gmail.com>
2025-08-02 18:41:28 +0630

    fix car detail banner

--- src/components/CarDetail/CarBanner.jsx
+++ src/components/CarDetail/CarBanner.jsx
@@ -4,10 +4,10 @@ function CarBanner({ image, pdf }) {
   return (
     <div className="w-full h-[400px] md:h-[450px] lg:h-[calc(100vh-80px)] relative">
       <div className="w-full h-full absolute">
-        <img src={image} className="w-full h-full" />
+        <img src={image} className="w-full h-full object-cover" />
       </div>
 
-      <div className="absolute bottom-[10px] md:bottom-[64px] right-[70%] md:right-0 w-[200px] md:w-[300px] translate-x-[50%] md:translate-x-0 md:right-0 z-10 p-0 md:pe-[64px] space-x-[24px]">
+      <div className="absolute bottom-[10px] md:bottom-[64px] right-[20%] md:right-0 w-[200px] md:w-[300px] translate-x-[50%] md:translate-x-0 md:right-0 z-10 p-0 md:pe-[64px] space-x-[24px]">
         <a
           href={pdf}
           target="_blank"
